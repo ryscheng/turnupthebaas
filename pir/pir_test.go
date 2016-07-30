a6721c28b6a4f43ff1866f1efe7eb98e6ba64c06
Will Scott <willscott@gmail.com>
2016-07-29 17:15:09 -0700

    fixed bug with sizing

--- pir/pir_test.go
+++ pir/pir_test.go
@@ -12,6 +12,7 @@ const cellCount = 1024
 const batchSize = 512
 
 type boringDB struct {
+	DataLength uint32
 	Data [][]byte
 }
 
@@ -20,19 +21,19 @@ func (b boringDB) Read(n uint32) []byte {
 }
 
 func (b boringDB) Length() uint32 {
-	return uint32(cellCount)
+	return b.DataLength
 }
 
 func TestSpeeds(t *testing.T) {
 	var counts = []int{1024, 1024 * 4, 1024 * 16, 1024 * 64, 1024 * 256}
 	var sizes = []int{1024, 2048, 4096}
 	var chunks = []int{16, 32, 64, 128, 256}
-	fmt.Printf("number of cells, size of cell, batched requests, nanoseconds")
+	fmt.Printf("number of cells, size of cell, batched requests, nanoseconds\n")
 	for _, count := range counts {
 		for _, size := range sizes {
 			for _, chunk := range chunks {
 				timing := SpeedFor(count, size, chunk)
-				fmt.Printf("%d,%d,%d,%d", count, size, chunk, timing.Nanoseconds())
+				fmt.Printf("%d,%d,%d,%d\n", count, size, chunk, timing.Nanoseconds())
 			}
 		}
 	}
@@ -41,6 +42,7 @@ func TestSpeeds(t *testing.T) {
 func SpeedFor(count int, size int, chunk int) time.Duration {
 	//Make Database
 	var db boringDB
+  db.DataLength = uint32(count)
 	db.Data = make([][]byte, count)
 	dataSize := count * size
 	fullData := make([]byte, dataSize)
@@ -90,6 +92,7 @@ func DoRead(b *testing.B, cellMultiple int) {
 	//Make Database
 	var db boringDB
 	theCellCount := cellCount * cellMultiple
+	db.DataLength = uint32(theCellCount)
 	db.Data = make([][]byte, theCellCount)
 	dataSize := cellSize * theCellCount
 	fullData := make([]byte, dataSize)
