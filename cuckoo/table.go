c58fc574632424384b18ae06104c60694b9e30c5
Raymond Cheng <me@raymondcheng.net>
2016-09-16 16:32:22 -0700

    first attempt at eviction loop

--- cuckoo/table.go
+++ cuckoo/table.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+const MAX_EVICTIONS int = 500
+
 type Comparable interface {
 	Compare(other Comparable) int
 }
@@ -75,25 +77,41 @@ func (t *Table) Contains(bucket1 int, bucket2 int, value Comparable) bool {
 	return result
 }
 
-/**
 // Inserts the value into the cuckoo table
-//   fails if either bucket already contains value
-// Returns true on success, false if not inserted
-// Even if false is returned, the underlying data structure might be different (e.g. rebuilt)
-func (t *Table) Insert(bucket1, bucket2, value Comparable) bool {
-	coin := rand.Int31()
-	ok := t.tryInsertToBucket(e.Bucket1, e)
-	if ok {
-		return true
-	}
-	ok = t.tryInsertToBucket(e.Bucket2, e)
-	if ok {
-		return true
-	}
-	// @todo Evict
+// Returns true on success, false on failure
+//   fails if insertion cannot complete because reached MAX_EVICTIONS
+//	 failures return an evicted value and must trigger a rebuild by the caller
+func (t *Table) Insert(bucket1 int, bucket2 int, value Comparable) (int, int, Comparable, bool) {
+	var ok bool
+	var nextBucket int
+	currBucketLoc := BucketLocation{bucket1, bucket2}
+	currVal := value
 
+	// Randomly select 1 bucket first
+	coin := t.rand.Int() % 2 // Coin could be -1, 0 or 1
+	if coin == 0 {
+		ok = t.tryInsertToBucket(bucket1, currBucketLoc, currVal)
+		nextBucket = bucket2
+	} else {
+		ok = t.tryInsertToBucket(bucket2, currBucketLoc, currVal)
+		nextBucket = bucket1
+	}
+	if ok {
+		return -1, -1, nil, true
+	}
+
+	// Then try the other bucket, starting the eviction loop
+	for i := 0; i < MAX_EVICTIONS; i++ {
+		nextBucket, currBucketLoc, currVal, ok = t.insertAndEvict(nextBucket, currBucketLoc, currVal)
+		if ok {
+			return -1, -1, nil, true
+		}
+	}
+
+	return currBucketLoc.Bucket1, currBucketLoc.Bucket2, currVal, false
 }
 
+/**
 // Removes the entry from the cuckoo table
 func (t *Table) Remove(bucket1 int, bucket2 int, target Comparable) {
 	t.removeFromBucket(target.Bucket1, target)
@@ -117,23 +135,17 @@ func (t *Table) isInBucket(bucketIndex int, value Comparable) bool {
 	return false
 }
 
-/**
-// Tries to inserts `target` into specified bucket
-// bucketIndex must be either `target.Bucket1` or `target.Bucket2` or nothing happens
+// Tries to inserts `bucketLoc, value` into specified bucket
 // If the bucket is already full, skip
 // Returns true if success, false if bucket already full
-func (t *Table) tryInsertToBucket(bucketIndex int, target *Entry) bool {
-	// Assert bucketIndex is part of `target`
-	if target.Bucket1 != bucketIndex && target.Bucket2 != bucketIndex {
-		return false
-	}
-
+func (t *Table) tryInsertToBucket(bucketIndex int, bucketLoc BucketLocation, value Comparable) bool {
 	// Search for an empty slot
 	bucket := t.buckets[bucketIndex]
-	for i, filled := range bucket.filled {
-		if !filled {
+	for i := 0; i < t.depth; i++ {
+		if !bucket.filled[i] {
+			bucket.data[i] = value
+			bucket.bucketLoc[i] = bucketLoc
 			bucket.filled[i] = true
-			bucket.entries[i] = target
 			return true
 		}
 	}
@@ -141,9 +153,38 @@ func (t *Table) tryInsertToBucket(bucketIndex int, target *Entry) bool {
 	return false
 }
 
-func (t *Table) evictAndInsert(bucketIndex int, target *Entry) *Entry {
+// Tries to insert `bucketLoc, value` into specified bucket
+// Returns -1, BucketLocation{}, nil, true if there's empty space and succeeds
+// Returns false if insertion triggered an eviction
+//   other values contain the evicted item's alternate bucket, BucketLocation pair, and value
+func (t *Table) insertAndEvict(bucketIndex int, bucketLoc BucketLocation, value Comparable) (int, BucketLocation, Comparable, bool) {
+	ok := t.tryInsertToBucket(bucketIndex, bucketLoc, value)
+	if ok {
+		return -1, BucketLocation{}, nil, true
+	}
+
+	// Eviction
+	randIndex := t.rand.Int() % t.depth
+	if randIndex < 0 {
+		randIndex *= -1
+	}
+	bucket := t.buckets[bucketIndex]
+	evictedBucketLoc := bucket.bucketLoc[randIndex]
+	evictedValue := bucket.data[randIndex]
+	bucket.bucketLoc[randIndex] = bucketLoc
+	bucket.data[randIndex] = value
+	bucket.filled[randIndex] = true
+	if bucketIndex == evictedBucketLoc.Bucket1 {
+		return evictedBucketLoc.Bucket2, evictedBucketLoc, evictedValue, false
+	} else if bucketIndex == evictedBucketLoc.Bucket2 {
+		return evictedBucketLoc.Bucket1, evictedBucketLoc, evictedValue, false
+	} else {
+		t.log.Fatalf("insertAndEvict: misplaced value! bucketIndex=%v does not match %v", bucketIndex, evictedBucketLoc)
+		return -1, BucketLocation{}, nil, false
+	}
 }
 
+/**
 // Removes all copies of `target` from the specified bucket
 // `target` matches against any entry where all fields match
 func (t *Table) removeFromBucket(bucketIndex int, target *Entry) bool {
