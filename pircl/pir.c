bdb8face4fbe54e12514f70534314d5105ee4080
Will Scott <willscott@gmail.com>
2016-10-05 17:59:22 -0700

    restructure pircl to process commands over a unix socket to be driven by clients.

--- pircl/pir.c
+++ pircl/pir.c
@@ -5,249 +5,309 @@
 #include <math.h>
 #include <time.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/un.h>
 #ifdef __APPLE__
 #include <OpenCL/opencl.h>
 #else
 #include <CL/cl.h>
 #endif
 
-#define CELL_COUNT (1024)
-#define CELL_LENGTH (1024)
-#define NUMBER_GROUPS (8)
-
 #define DATA_TYPE unsigned long
 
+#define SOCKET_NAME "pir.socket"
+
 ////////////////////////////////////////////////////////////////////////////////
+//
+// PIR Server.
+//
+// Communication occurs via a socket. several commands are present.
+// 1. read. expects an input pir vector.
+// 2. configure. sets parameters for batching of groups.
+// 3. write. updates the database.
+////////////////////////////////////////////////////////////////////////////////
+int cell_length;
+int cell_count;
+int batch_size;
+size_t workgroup_size;
+char* invector;
+DATA_TYPE* database;
+DATA_TYPE* output;
+cl_context context;                 // compute context
+cl_command_queue commands;          // compute command queue
+cl_program program;                 // compute program
+cl_kernel kernel;                   // compute kernel
+cl_mem gpu_db;                      // device memory used for the database
+cl_mem gpu_input;
+cl_mem gpu_output;
+
+int configure(int, int, int);
+int do_write(DATA_TYPE*);
+DATA_TYPE* do_read(char*);
+
 int main()
 {
-    int err;                            // error code returned from api calls
+    int err;
 
-    DATA_TYPE* database = malloc(CELL_COUNT * CELL_LENGTH * sizeof(DATA_TYPE));
-    char invector[CELL_COUNT * NUMBER_GROUPS]; // input vector given to device
-    DATA_TYPE outdata[CELL_LENGTH * NUMBER_GROUPS];   // results returned from device
-    unsigned int correct;               // number of correct results returned
-
-    size_t global;                      // global domain size for our calculation
-    size_t local;                       // local domain size for our calculation
-
-    cl_device_id device_id;             // compute device id
-    cl_context context;                 // compute context
-    cl_command_queue commands;          // compute command queue
-    cl_program program;                 // compute program
-    cl_kernel kernel;                   // compute kernel
-
-    cl_mem db;                          // device memory used for the database
-    cl_mem input;                       // device memory used for the input array
-    cl_mem output;                      // device memory used for the output array
-
-    cl_ulong buf_max_size;
-    cl_ulong dev_mem_size;
-
-    int i = 0;
-    int j = 0;
-    unsigned int count = CELL_COUNT;
-    unsigned int dbsize = CELL_COUNT * CELL_LENGTH;
-    unsigned int block_size = CELL_LENGTH;
-
-    // Randomize input vectors.
-    for (i = 0; i < CELL_COUNT * NUMBER_GROUPS; i++) {
-      invector[i] = (rand() % 2 == 1);
+    int socket_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    int client_sock;
+    if (socket_fd == -1) {
+      printf("Error: Failed to create sockets!\n");
+      return EXIT_FAILURE;
     }
 
-    // Connect to a compute device
-    //
-    cl_platform_id cl_platform;
-    err = clGetPlatformIDs(1, &cl_platform, NULL);
+    // Bind the socket for communication.
+    unlink(SOCKET_NAME);
+    struct sockaddr_un socket_name;
+    memset(&socket_name, 0, sizeof(struct sockaddr_un));
+    socket_name.sun_family = AF_UNIX;
+    strncpy(socket_name.sun_path, SOCKET_NAME, sizeof(socket_name.sun_path) - 1);
+    err = bind(socket_fd, (const struct sockaddr *) &socket_name, sizeof(struct sockaddr_un));
+    if (err == -1) {
+      printf("Error: Failed to bind socket!\n");
+      return EXIT_FAILURE;
+    }
 
-    int gpu = 1;
-    err = clGetDeviceIDs(cl_platform, gpu ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU, 1, &device_id, NULL);
-    if (err != CL_SUCCESS)
-    {
-        printf("Error: Failed to create a device group!\n");
+    err = listen(socket_fd, 1);
+    if (err == -1) {
+      printf("Error: Failed to listen on socket!\n");
+      return EXIT_FAILURE;
+    }
+
+
+    char next_command;
+    int ret;
+    int configuration_params[3];
+    for (;;) {
+      client_sock = accept(socket_fd, NULL, NULL);
+      if (client_sock == -1) {
+        printf("Error: failed to accept client on listening socket!\n");
         return EXIT_FAILURE;
-    }
-
-    // Create a compute context
-    //
-    context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
-    if (!context)
-    {
-        printf("Error: Failed to create a compute context!\n");
-        return EXIT_FAILURE;
-    }
-
-    // Create a command commands
-    //
-    commands = clCreateCommandQueue(context, device_id, 0, &err);
-    if (!commands)
-    {
-        printf("Error: Failed to create a command commands!\n");
-        return EXIT_FAILURE;
-    }
-
-    // Map in kernel source code
-    struct stat kernelStat;
-    int kernelHandle = open("kernel.c", O_RDONLY);
-    int status = fstat (kernelHandle, &kernelStat);
-    char* kernelSource = (char *) mmap(NULL, kernelStat.st_size, PROT_READ, MAP_PRIVATE, kernelHandle, 0);
-
-    // Create the compute program from the source buffer
-    //
-    program = clCreateProgramWithSource(context, 1, (const char **) & kernelSource, NULL, &err);
-    if (!program)
-    {
-        printf("Error: Failed to create compute program!\n");
-        return EXIT_FAILURE;
-    }
-
-    // Build the program executable
-    //
-    err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-    if (err != CL_SUCCESS)
-    {
-        size_t len;
-        char buffer[2048];
-
-        printf("Error: Failed to build program executable!\n");
-        clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &len);
-        printf("%s\n", buffer);
-        exit(1);
-    }
-
-    // Create the compute kernels in the program we wish to run
-    //
-    kernel = clCreateKernel(program, "pir", &err);
-    if (!kernel || err != CL_SUCCESS)
-    {
-        printf("Error: Failed to create compute kernel!\n");
-        exit(1);
-    }
-
-    // learn about the device memory size:
-    err = clGetDeviceInfo(device_id, CL_DEVICE_MAX_MEM_ALLOC_SIZE, sizeof(cl_ulong), &buf_max_size, NULL);
-    err = clGetDeviceInfo(device_id, CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(cl_ulong), &dev_mem_size, NULL);
-    printf("Global size is %lu. Maximum buffer on device can be %lu.\n", dev_mem_size, buf_max_size);
-
-    // Get the maximum work group size for executing the kernel on the device
-    //
-    err = clGetKernelWorkGroupInfo(kernel, device_id, CL_KERNEL_WORK_GROUP_SIZE, sizeof(local), &local, NULL);
-    if (err != CL_SUCCESS)
-    {
-        printf("Error: Failed to retrieve kernel work group info! %d\n", err);
-        exit(1);
-    }
-
-    // Create the arrays in device memory for our calculation
-    //
-    db = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(DATA_TYPE) * CELL_COUNT * CELL_LENGTH, NULL, NULL);
-    input = clCreateBuffer(context,  CL_MEM_READ_ONLY, CELL_COUNT * NUMBER_GROUPS, NULL, NULL);
-    output = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(DATA_TYPE) * CELL_LENGTH * NUMBER_GROUPS, NULL, NULL);
-    if (!input || !output || !db)
-    {
-        printf("Error: Failed to allocate device memory!\n");
-        exit(1);
-    }
-
-    // Initialize the database.
-    printf("Allocating & randomizing memory for the database.\n");
-    for(i = 0; i < dbsize; i++)
-        database[i] = rand();
-
-    err = clEnqueueWriteBuffer(commands, db, CL_TRUE, 0, sizeof(DATA_TYPE) * dbsize, database, 0, NULL, NULL);
-    if (err != CL_SUCCESS)
-    {
-        printf("Error: Failed to write db to device!\n");
-        exit(1);
-    }
-
-    printf("Done.\n");
-
-    // Write our data set into the input array in device memory
-    err = clEnqueueWriteBuffer(commands, input, CL_TRUE, 0,  CELL_COUNT * NUMBER_GROUPS, invector, 0, NULL, NULL);
-    if (err != CL_SUCCESS)
-    {
-        printf("Error: Failed to write to source array!\n");
-        exit(1);
-    }
-
-    // Set the arguments to our compute kernel
-    //
-    err = 0;
-    err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &db);
-    err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &input);
-    err |= clSetKernelArg(kernel, 2, sizeof(unsigned long) * CELL_LENGTH, NULL);
-    err |= clSetKernelArg(kernel, 3, sizeof(unsigned int), &dbsize);
-    err |= clSetKernelArg(kernel, 4, sizeof(unsigned int), &block_size);
-    err |= clSetKernelArg(kernel, 5, sizeof(cl_mem), &output);
-    if (err != CL_SUCCESS)
-    {
-        printf("Error: Failed to set kernel arguments! %d\n", err);
-        exit(1);
-    }
-
-    // Execute the kernel over the entire range of our 1d input data set
-    // using the maximum number of work group items for this device
-    //
-    global = local * NUMBER_GROUPS;
-    err = clEnqueueNDRangeKernel(commands, kernel, 1, NULL, &global, &local, 0, NULL, NULL);
-    if (err)
-    {
-        printf("Error: Failed to execute kernel!\n");
-        return EXIT_FAILURE;
-    }
-    // Wait for the command commands to get serviced before stage 2
-    //
-    clFinish(commands);
-
-    // Read back the results from the device to verify the output
-    //
-    err = clEnqueueReadBuffer( commands, output, CL_TRUE, 0, sizeof(DATA_TYPE) * CELL_LENGTH * NUMBER_GROUPS, outdata, 0, NULL, NULL );
-    if (err != CL_SUCCESS)
-    {
-        printf("Error: Failed to read output array! %d\n", err);
-        exit(1);
-    }
-
-    printf("Recomputing to make sure these look right.\n");
-
-    // Validate our results
-    //
-    correct = 0;
-    for(i = 0; i < NUMBER_GROUPS; i += 1) {
-      DATA_TYPE acc[CELL_LENGTH];
-      int bit;
-      for (j = 0; j < CELL_LENGTH; j++)
-        acc[j] = 0;
-      for (j = 0; j < dbsize; j++) {
-        bit = j / CELL_LENGTH;
-        if (invector[(CELL_COUNT * i) + bit] != 0)
-          acc[j % CELL_LENGTH] ^= database[j];
       }
-      for (j = 0; j < CELL_LENGTH; j++)
-        if(outdata[(CELL_COUNT * i) + j] != acc[j])
-          goto OUTERCONTINUE;
-      correct++;
-      OUTERCONTINUE:
-      ;
+      for(;;) {
+        /* Wait for next data packet. */
+        ret = read(client_sock, &next_command, 1);
+        if (ret == -1) {
+          printf("Client disconnected.\n");
+          break;
+        }
+        if (next_command == '1') { //read
+          ret = read(client_sock, invector, cell_count * batch_size);
+          if (ret == -1) {
+            printf("Failed to read configuration.\n");
+            break;
+          }
+          output = do_read(invector);
+          ret = write(client_sock, output, cell_length * batch_size);
+          if (ret == -1) {
+            printf("Failed to write response.\n");
+            break;
+          }
+        } else if (next_command == '2') { //configure
+          ret = read(client_sock, configuration_params, 3*sizeof(int));
+          if (ret == -1) {
+            printf("Failed to read configuration.\n");
+            break;
+          }
+          configure(configuration_params[0], configuration_params[1], configuration_params[2]);
+        } else if (next_command == '3') { // write
+          int nread = 0;
+          if (database != NULL) {
+            free(database);
+          }
+          database = malloc(cell_length * cell_count);
+          while(nread < cell_length * cell_count) {
+            ret = read(client_sock, database + nread, (cell_length * cell_count - nread));
+            if (ret == -1) {
+              printf("failed to read new DB.\n");
+              exit(1);
+            }
+            nread += ret;
+          }
+          do_write(database);
+        }
+      }
     }
+}
 
-    // Print a brief summary detailing the results
-    //
-    if (correct > 0)
-      printf("Computed the correct value!\n");
+int configure(int n_cell_length, int n_cell_count, int n_batch_size) {
+  int err;
 
-    // Shutdown and cleanup
-    //
-    clReleaseMemObject(db);
-    clReleaseMemObject(input);
-    clReleaseMemObject(output);
+  cell_length = n_cell_length;
+  cell_count = n_cell_count;
+  batch_size = n_batch_size;
+
+  if (output != NULL) {
+    free(output);
+    free(invector);
+    invector = NULL;
+    output = NULL;
+  }
+  output = malloc(cell_length * batch_size);
+  invector = malloc(cell_count * batch_size);
+
+  if (context != NULL) {
     clReleaseProgram(program);
     clReleaseKernel(kernel);
     clReleaseCommandQueue(commands);
     clReleaseContext(context);
+  }
 
-    return 0;
+  cl_device_id device_id;             // compute device id
+  cl_platform_id cl_platform;
+
+  // Connect to a compute device
+  err = clGetPlatformIDs(1, &cl_platform, NULL);
+  int gpu = 1;
+  err = clGetDeviceIDs(cl_platform, gpu ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU, 1, &device_id, NULL);
+  if (err != CL_SUCCESS)
+  {
+      printf("Error: Failed to create a device group!\n");
+      return EXIT_FAILURE;
+  }
+
+  // Create a compute context
+  context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
+  if (!context)
+  {
+      printf("Error: Failed to create a compute context!\n");
+      return EXIT_FAILURE;
+  }
+
+  // Create a command queue
+  commands = clCreateCommandQueue(context, device_id, 0, &err);
+  if (!commands)
+  {
+      printf("Error: Failed to create a command commands!\n");
+      return EXIT_FAILURE;
+  }
+
+  // Map in kernel source code
+  struct stat kernelStat;
+  int kernelHandle = open("kernel.c", O_RDONLY);
+  int status = fstat (kernelHandle, &kernelStat);
+  char* kernelSource = (char *) mmap(NULL, kernelStat.st_size, PROT_READ, MAP_PRIVATE, kernelHandle, 0);
+
+  // Create the compute program from the source buffer
+  //
+  program = clCreateProgramWithSource(context, 1, (const char **) & kernelSource, NULL, &err);
+  if (!program)
+  {
+      printf("Error: Failed to create compute program!\n");
+      return EXIT_FAILURE;
+  }
+
+  // Build the program executable
+  //
+  err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+  if (err != CL_SUCCESS)
+  {
+      size_t len;
+      char buffer[2048];
+
+      printf("Error: Failed to build program executable!\n");
+      clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &len);
+      printf("%s\n", buffer);
+      exit(1);
+  }
+
+  // Create the compute kernels in the program we wish to run
+  //
+  kernel = clCreateKernel(program, "pir", &err);
+  if (!kernel || err != CL_SUCCESS)
+  {
+      printf("Error: Failed to create compute kernel!\n");
+      exit(1);
+  }
+
+  // learn about the device memory size:
+  //err = clGetDeviceInfo(device_id, CL_DEVICE_MAX_MEM_ALLOC_SIZE, sizeof(cl_ulong), &buf_max_size, NULL);
+  //err = clGetDeviceInfo(device_id, CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(cl_ulong), &dev_mem_size, NULL);
+  //printf("Global size is %lu. Maximum buffer on device can be %lu.\n", dev_mem_size, buf_max_size);
+
+  // Get the maximum work group size for executing the kernel on the device
+  //
+  err = clGetKernelWorkGroupInfo(kernel, device_id, CL_KERNEL_WORK_GROUP_SIZE, sizeof(workgroup_size), &workgroup_size, NULL);
+  if (err != CL_SUCCESS)
+  {
+      printf("Error: Failed to retrieve kernel work group info! %d\n", err);
+      exit(1);
+  }
+
+  if (gpu_db != NULL) {
+    clReleaseMemObject(gpu_db);
+    clReleaseMemObject(gpu_input);
+    clReleaseMemObject(gpu_output);
+  }
+  gpu_db = clCreateBuffer(context, CL_MEM_READ_ONLY, cell_length * cell_count, NULL, NULL);
+  gpu_input = clCreateBuffer(context,  CL_MEM_READ_ONLY, cell_count * batch_size, NULL, NULL);
+  gpu_output = clCreateBuffer(context, CL_MEM_WRITE_ONLY, cell_length * batch_size, NULL, NULL);
+
+  if (!gpu_db || !gpu_input || !gpu_output) {
+      printf("Error: Failed to allocate device memory!\n");
+      exit(1);
+  }
+
+  // set kernel args.
+  unsigned int db_cnt = cell_count * cell_length / sizeof(DATA_TYPE);
+  unsigned int db_cell_cnt = cell_length / sizeof(DATA_TYPE);
+  err = 0;
+  err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &gpu_db);
+  err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &gpu_input);
+  err |= clSetKernelArg(kernel, 2, cell_length, NULL);
+  err |= clSetKernelArg(kernel, 3, sizeof(unsigned int), &db_cnt);
+  err |= clSetKernelArg(kernel, 4, sizeof(unsigned int), &db_cell_cnt);
+  err |= clSetKernelArg(kernel, 5, sizeof(cl_mem), &gpu_output);
+  if (err != CL_SUCCESS)
+  {
+      printf("Error: Failed to set kernel arguments! %d\n", err);
+      exit(1);
+  }
+  printf("Reconfigured. Database now %d items of %d bytes. Batches of %d requests.\n", cell_count, cell_length, batch_size);
+}
+
+int do_write(DATA_TYPE* db) {
+  int err;
+  err = clEnqueueWriteBuffer(commands, gpu_db, CL_TRUE, 0, cell_length * cell_count, db, 0, NULL, NULL);
+  if (err != CL_SUCCESS) {
+    printf("Error: Failed to write db to device!\n");
+    exit(1);
+  }
+  printf("Database updated.\n");
+}
+
+DATA_TYPE* do_read(char* invector) {
+  int err;
+
+  // Write input vector.
+  err = clEnqueueWriteBuffer(commands, gpu_input, CL_TRUE, 0, cell_count * batch_size, invector, 0, NULL, NULL);
+  if (err != CL_SUCCESS)
+  {
+      printf("Error: Failed to write to source array!\n");
+      exit(1);
+  }
+
+  // Execute the kernel over the entire range of our 1d input data set
+  // using the maximum number of work group items for this device
+  //
+  size_t totaljobs = workgroup_size * batch_size;
+  err = clEnqueueNDRangeKernel(commands, kernel, 1, NULL, &totaljobs, &workgroup_size, 0, NULL, NULL);
+  if (err) {
+      printf("Error: Failed to execute kernel!\n");
+      exit(1);
+  }
+
+  // Read back the results from the device to verify the output
+  //
+  err = clEnqueueReadBuffer(commands, gpu_output, CL_TRUE, 0, cell_length * batch_size, output, 0, NULL, NULL );
+  if (err != CL_SUCCESS)
+  {
+      printf("Error: Failed to read output array! %d\n", err);
+      exit(1);
+  }
+  printf("Read Batch.\n");
+  return output;
 }
