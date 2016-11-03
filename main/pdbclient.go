e8cf3ad0428058e4c047551134c213872725f286
Will Scott <willscott@gmail.com>
2016-11-03 10:48:57 -0700

    load config from a file.

--- main/pdbclient.go
+++ main/pdbclient.go
@@ -15,21 +15,11 @@ func main() {
 	// Config
 	trustDomainConfig0 := common.NewTrustDomainConfig("t0", "localhost:9000", true, false)
 	trustDomainConfig1 := common.NewTrustDomainConfig("t1", "localhost:9001", true, false)
-	globalConfig := common.GlobalConfig{
-		100,         // NumBuckets
-		2,           // BucketDepth
-		100,         // WindowSize
-		1024,        // DataSize
-		1,           // ReadBatch
-		0.001,       // Bloom FP Rate
-		0.90,        // LoadFactor
-		0.05,        // LoadFactorStep
-		time.Second, // WriteInterval
-		time.Second, // ReadInterval
-		[]*common.TrustDomainConfig{trustDomainConfig0, trustDomainConfig1},
-	}
+	globalConfig := common.GlobalConfigFromFile("globalconfig.json")
+	globalConfig.TrustDomains = []*common.TrustDomainConfig{trustDomainConfig0, trustDomainConfig1}
 
-	c := libpdb.NewClient("c", globalConfig, common.NewLeaderRpc("c0->t0", trustDomainConfig0))
+
+	c := libpdb.NewClient("c", *globalConfig, common.NewLeaderRpc("c0->t0", trustDomainConfig0))
 	c.Ping()
 
   //for {
--- main/simple-centralized.go
+++ main/simple-centralized.go
@@ -19,33 +19,22 @@ func main() {
 	// Config
 	trustDomainConfig0 := common.NewTrustDomainConfig("t0", "localhost:9000", true, false)
 	trustDomainConfig1 := common.NewTrustDomainConfig("t1", "localhost:9001", true, false)
-	globalConfig := common.GlobalConfig{
-		100,         // NumBuckets
-		2,           // BucketDepth
-		100,         // WindowSize
-		1024,        // DataSize
-		1,           // ReadBatch
-		0.001,       // Bloom FP Rate
-		0.90,        // LoadFactor
-		0.05,        // LoadFactorStep
-		time.Second, // WriteInterval
-		time.Second, // ReadInterval
-		[]*common.TrustDomainConfig{trustDomainConfig0, trustDomainConfig1},
-	}
+	globalConfig := common.GlobalConfigFromFile("globalconfig.json")
+	globalConfig.TrustDomains = []*common.TrustDomainConfig{trustDomainConfig0, trustDomainConfig1}
 
 	// Trust Domain 1
-	t1 := server.NewCentralized("t1", globalConfig, nil, false)
+	t1 := server.NewCentralized("t1", *globalConfig, nil, false)
 	s["t1"] = server.NewNetworkRpc(t1, 9001)
 
 	// Trust Domain 0
 	//t0 := server.NewCentralized("t0", globalConfig, t1, true)
-	t0 := server.NewCentralized("t0", globalConfig, common.NewFollowerRpc("t0->t1", trustDomainConfig1), true)
+	t0 := server.NewCentralized("t0", *globalConfig, common.NewFollowerRpc("t0->t1", trustDomainConfig1), true)
 	s["t0"] = server.NewNetworkRpc(t0, 9000)
 
 	// Client
 	//c0 := libpdb.NewClient("c0", globalConfig, t0)
 	//c1 := libpdb.NewClient("c1", globalConfig, t0)
-	c0 := libpdb.NewClient("c0", globalConfig, common.NewLeaderRpc("c0->t0", trustDomainConfig0))
+	c0 := libpdb.NewClient("c0", *globalConfig, common.NewLeaderRpc("c0->t0", trustDomainConfig0))
 	c0.Ping()
 	//c1.Ping()
 	time.Sleep(10 * time.Second)
