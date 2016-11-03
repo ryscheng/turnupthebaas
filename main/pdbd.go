1eed8f8cb4ffb2f7efc6264c1d302c9b8b810dfc
Raymond Cheng <me@raymondcheng.net>
2016-11-03 13:05:07 -0700

    internal IPS

--- main/pdbd.go
+++ main/pdbd.go
@@ -19,8 +19,10 @@ func main() {
 	// Config
 	//trustDomainConfig0 := common.NewTrustDomainConfig("t0", "localhost:9000", true, false)
 	//trustDomainConfig1 := common.NewTrustDomainConfig("t1", "localhost:9001", true, false)
-	trustDomainConfig0 := common.NewTrustDomainConfig("t0", "35.163.10.49:9000", true, false)
-	trustDomainConfig1 := common.NewTrustDomainConfig("t1", "35.163.8.29:9000", true, false)
+	//trustDomainConfig0 := common.NewTrustDomainConfig("t0", "35.163.10.49:9000", true, false)
+	trustDomainConfig0 := common.NewTrustDomainConfig("t0", "172.30.2.10:9000", true, false)
+	//trustDomainConfig1 := common.NewTrustDomainConfig("t1", "35.163.8.29:9000", true, false)
+	trustDomainConfig1 := common.NewTrustDomainConfig("t1", "172.30.2.221:9000", true, false)
 	globalConfig := common.GlobalConfigFromFile("globalconfig.json")
 	globalConfig.TrustDomains = []*common.TrustDomainConfig{trustDomainConfig0, trustDomainConfig1}
 
