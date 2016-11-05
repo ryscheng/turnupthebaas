ffe97602c1284693de1c92efae96ac3055dd9c6b
Will Scott <willscott@gmail.com>
2016-11-05 15:23:47 -0700

    centralized test runs

--- server/centralized_test.go
+++ server/centralized_test.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rand"
 	"github.com/ryscheng/pdb/common"
 	"github.com/ryscheng/pdb/libpdb"
+	"github.com/ryscheng/pdb/pir"
 	"testing"
 	"time"
 )
@@ -15,6 +16,7 @@ func BenchmarkWrite(b *testing.B) {
 	globalConfig.NumBuckets = 128
 	globalConfig.BucketDepth = 4
 	globalConfig.DataSize = 256
+	globalConfig.MaxLoadFactor = 0.90
 	globalConfig.BloomFalsePositive = 0.1
 
 	plaintext := make([]byte, globalConfig.DataSize, globalConfig.DataSize)
@@ -33,8 +35,17 @@ func BenchmarkWrite(b *testing.B) {
 	}
 
 	var reply common.WriteReply
-	t1 := NewCentralized("t1", globalConfig, nil, false)
-	t0 := NewCentralized("t0", globalConfig, t1, true)
+	t1s := getSocket()
+	t1c := make(chan int)
+	go pir.CreateMockServer(t1c, t1s)
+	<-t1c
+	t1 := NewCentralized("t1", t1s, globalConfig, nil, false)
+
+	t0s := getSocket()
+	t0c := make(chan int)
+	go pir.CreateMockServer(t0c, t0s)
+	<-t0c
+	t0 := NewCentralized("t0", t0s, globalConfig, t1, true)
 
 	// Start timing
 	b.ResetTimer()
@@ -42,4 +53,6 @@ func BenchmarkWrite(b *testing.B) {
 		_ = t0.Write(args, &reply)
 	}
 
+	t1c <- 1
+	t0c <- 1
 }
