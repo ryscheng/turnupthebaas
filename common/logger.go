2d1560ba2a6d40579939e1273353295914f3f077
Raymond Cheng <me@raymondcheng.net>
2016-11-05 13:28:03 -0700

    adding example for disabling a log

--- common/logger.go
+++ common/logger.go
@@ -3,6 +3,7 @@ package common
 import (
 	"log"
 	"os"
+	//"io/ioutil"
 )
 
 type Logger struct {
@@ -17,7 +18,9 @@ func NewLogger(name string) *Logger {
 	l := &Logger{}
 	l.name = name
 	l.Trace = log.New(os.Stdout, "["+name+":TRACE] ", log.Ldate|log.Ltime|log.Lshortfile)
+	//l.Trace = log.New(ioutil.Discard, "["+name+":TRACE] ", log.Ldate|log.Ltime|log.Lshortfile)
 	l.Info = log.New(os.Stdout, "["+name+":INFO] ", log.Ldate|log.Ltime|log.Lshortfile)
+	//l.Info = log.New(ioutil.Discard, "["+name+":INFO] ", log.Ldate|log.Ltime|log.Lshortfile)
 	l.Warn = log.New(os.Stderr, "["+name+":WARN] ", log.Ldate|log.Ltime|log.Lshortfile)
 	l.Error = log.New(os.Stderr, "["+name+":ERROR] ", log.Ldate|log.Ltime|log.Lshortfile)
 	return l
