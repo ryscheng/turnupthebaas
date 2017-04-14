9855c1e198a2416ac32165fee3e63a93d971632e
Will Scott <willscott@gmail.com>
2017-04-14 08:07:33 -0700

    extract the read timeout multiple to a const

--- cli/talekclient/main.go
+++ cli/talekclient/main.go
@@ -12,6 +12,8 @@ import (
 	"github.com/privacylab/talek/libtalek"
 )
 
+const readTimeoutMultiple = 5
+
 var config = flag.String("config", "talek.conf", "Client configuration for talek")
 var create = flag.Bool("create", false, "Create a new talek handle")
 var share = flag.String("share", "", "Create a read-only version of the topic for sharing")
@@ -80,7 +82,7 @@ func main() {
 		fmt.Fprintf(os.Stderr, "No Read or Write operation requested. Closing.")
 	} else {
 		msgs := client.Poll(&topic.Handle)
-		timeout := time.After(config.ReadInterval * 5)
+		timeout := time.After(config.ReadInterval * readTimeoutMultiple)
 		select {
 		case next := <-msgs:
 			fmt.Println(next)
