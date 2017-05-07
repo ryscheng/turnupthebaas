0e65e3466aeb2aa9022ba39a050c4f89b1271cfd
Raymond Cheng <me@raymondcheng.net>
2017-05-07 12:59:37 -0700

    update types in cuda kernel

--- pir/cuda_modules/pir.cu
+++ pir/cuda_modules/pir.cu
@@ -14,6 +14,8 @@
 extern "C" {
 #endif
 
+typedef char int8_cu;
+typedef unsigned char uint8_cu;
 typedef long int int32_cu;
 typedef unsigned long int uint32_cu;
 typedef long long int int64_cu;
@@ -23,7 +25,7 @@ typedef unsigned long long int uint64_cu;
 // CUDA Kernel
 __global__
 void pir(DATA_TYPE* db,
-        char* reqs,
+        uint8_cu* reqs,
         DATA_TYPE* output,
         //DATA_TYPE* scratch,
         uint32_cu batchSize,
@@ -34,17 +36,17 @@ void pir(DATA_TYPE* db,
         uint32_cu scratchSize) {
   //int localIndex = threadIdx.x;
   //int groupIndex = blockIdx.x;
-  int globalIndex = threadIdx.x + (blockIdx.x * blockDim.x);
+  uint32_cu globalIndex = threadIdx.x + (blockIdx.x * blockDim.x);
 
   if (globalIndex >= globalSize) {
     return;
   }
   // Iterate over all buckets, xor data into my result
   DATA_TYPE result = 0;
-  int reqIndex = (globalIndex / bucketSize) * reqLength;
-  int offset = globalIndex % bucketSize;
-  unsigned char reqBit;
-  for (int i = 0; i < numBuckets; i++) {
+  uint32_cu reqIndex = (globalIndex / bucketSize) * reqLength;
+  uint32_cu offset = globalIndex % bucketSize;
+  uint8_cu reqBit;
+  for (uint32_cu i = 0; i < numBuckets; i++) {
     reqBit = reqs[reqIndex + (i/8)] & (1 << (i%8));
     if (reqBit > 0) {
       result ^= db[i*bucketSize+offset];
