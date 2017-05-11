509c243f1e7d3241bacfee7ee6fc7ca5fadcf534
Raymond Cheng <me@raymondcheng.net>
2017-05-11 14:09:05 -0700

    rename ala lint

--- server/coordinator/interface.go
+++ server/coordinator/interface.go
@@ -4,8 +4,8 @@ import (
 	"github.com/privacylab/talek/common"
 )
 
-// CoordinatorInterface is the interface to the central coordinator
-type CoordinatorInterface interface {
+// Interface is the interface to the central coordinator
+type Interface interface {
 	GetInfo(args *interface{}, reply *GetInfoReply) error
 	GetConfig(args *interface{}, reply *common.Config) error
 	Commit(args *CommitArgs, reply *CommitReply) error
--- server/coordinator/rpc_stub.go
+++ server/coordinator/rpc_stub.go
@@ -6,8 +6,8 @@ import (
 	"github.com/privacylab/talek/common"
 )
 
-// CoordinatorRPC is a stub for RPCs to the central coordinator server.
-type CoordinatorRPC struct {
+// RPCStub is a stub for RPCs to the central coordinator server.
+type RPCStub struct {
 	log     *common.Logger
 	name    string
 	address string
@@ -16,8 +16,8 @@ type CoordinatorRPC struct {
 }
 
 // NewCoordinatorRPC instantiates a client stub
-func NewCoordinatorRPC(name string, address string) *CoordinatorRPC {
-	c := &CoordinatorRPC{}
+func NewRPCStub(name string, address string) *RPCStub {
+	c := &RPCStub{}
 	c.log = common.NewLogger(name)
 	c.name = name
 	c.address = address
@@ -26,7 +26,7 @@ func NewCoordinatorRPC(name string, address string) *CoordinatorRPC {
 }
 
 // Close will close the RPC client
-func (c *CoordinatorRPC) Close() error {
+func (c *RPCStub) Close() error {
 	if c.client != nil {
 		c.lastErr = c.client.Close()
 		c.client = nil
@@ -36,24 +36,24 @@ func (c *CoordinatorRPC) Close() error {
 }
 
 // GetName returns the name of the leader.
-func (c *CoordinatorRPC) GetInfo(_ *interface{}, reply *GetInfoReply) error {
+func (c *RPCStub) GetInfo(_ *interface{}, reply *GetInfoReply) error {
 	c.client, c.lastErr = common.RPCCall(c.client, c.address, "Coordinator.GetInfo", nil, reply)
 	return c.lastErr
 }
 
 // GetConfig tells the client about current config.
-func (c *CoordinatorRPC) GetConfig(_ *interface{}, reply *common.Config) error {
+func (c *RPCStub) GetConfig(_ *interface{}, reply *common.Config) error {
 	c.client, c.lastErr = common.RPCCall(c.client, c.address, "Coordinator.GetConfig", nil, reply)
 	return c.lastErr
 }
 
-func (c *CoordinatorRPC) Commit(args *CommitArgs, reply *CommitReply) error {
+func (c *RPCStub) Commit(args *CommitArgs, reply *CommitReply) error {
 	c.client, c.lastErr = common.RPCCall(c.client, c.address, "Coordinator.Commit", args, reply)
 	return c.lastErr
 }
 
 // GetUpdates provides the global interest vector.
-func (c *CoordinatorRPC) GetUpdates(args *common.GetUpdatesArgs, reply *common.GetUpdatesReply) error {
+func (c *RPCStub) GetUpdates(args *common.GetUpdatesArgs, reply *common.GetUpdatesReply) error {
 	c.client, c.lastErr = common.RPCCall(c.client, c.address, "Coordinator.GetUpdates", args, reply)
 	return c.lastErr
 }
