ec50049c80b6ee90e9f55a4aa3362eb70d48cd2c
Raymond Cheng <me@raymondcheng.net>
2017-05-12 15:11:31 -0700

    setup triggers for a layout build

--- server/coordinator/server.go
+++ server/coordinator/server.go
@@ -2,6 +2,7 @@ package coordinator
 
 import (
 	"sync/atomic"
+	"time"
 
 	"github.com/privacylab/talek/common"
 	//"github.com/privacylab/talek/cuckoo"
@@ -12,24 +13,37 @@ import (
 type Server struct {
 	/** Private State **/
 	// Static
-	log  *common.Logger
-	name string
+	log            *common.Logger
+	name           string
+	buildThreshold uint64
+	buildInterval  time.Duration
 
 	// Thread-safe
-	config    atomic.Value  // Config
-	commitLog []*CommitArgs // Append and read only
+	config        atomic.Value  // Config
+	commitLog     []*CommitArgs // Append and read only
+	numNewCommits uint64
+	timeLastBuild time.Time
+	buildCount    uint64
 
 	// Channels
 	commitChan chan *CommitArgs
 }
 
 // NewServer creates a new Centralized talek server.
-func NewServer(name string, config common.Config) *Server {
+func NewServer(name string, config common.Config, buildThreshold uint64, buildInterval time.Duration) *Server {
 	s := &Server{}
 	s.log = common.NewLogger(name)
 	s.name = name
-	s.config.Store(config)
+	s.buildThreshold = buildThreshold
+	s.buildInterval = buildInterval
 
+	s.config.Store(config)
+	s.commitLog = make([]*CommitArgs, 0)
+	s.numNewCommits = 0
+	s.timeLastBuild = time.Now()
+	s.buildCount = 0
+
+	go s.processCommits()
 	return s
 }
 
@@ -78,12 +92,51 @@ func (s *Server) Close() {
 // processCommits will read from s.commitChan and properly trigger work
 func (s *Server) processCommits() {
 	var commit *CommitArgs
-	//conf := s.config.Load().(common.Config)
+	conf := s.config.Load().(common.Config)
+	windowSize := conf.WindowSize()
+	tick := time.After(s.buildInterval)
+
+	triggerBuild := func() {
+		// Garbage collect old items
+		idx := 0
+		if len(s.commitLog) > windowSize {
+			idx = len(s.commitLog) - windowSize
+		}
+		s.commitLog = s.commitLog[idx:]
+		// Spawn build goroutine
+		go s.buildLayout(s.buildCount, s.commitLog[:])
+		// Reset state
+		s.numNewCommits = 0
+		s.buildCount += 1
+	}
 
 	for {
 		select {
+		// Handle new commits
 		case commit = <-s.commitChan:
 			s.commitLog = append(s.commitLog, commit)
+			s.numNewCommits += 1
+			// Trigger build if over threshold
+			if s.numNewCommits > s.buildThreshold {
+				triggerBuild()
+			}
+			continue
+		// Periodically trigger a build
+		case <-tick:
+			triggerBuild()
+			tick = time.After(s.buildInterval) // Re-up timer
+			continue
 		}
 	}
+}
+
+func (s *Server) buildLayout(buildId uint64, commitLog []*CommitArgs) {
+	// Construct cuckoo table layout
+
+	// Construct global interest vector
+
+	// Push layout to replicas
+
+	// Push global interest vector to global frontends
+
 }
