60472ca16647e5e96bb9a1a7ea81f420b063ae43
Will Scott <will@cypherpunk.email>
2017-05-13 13:46:03 -0400

    include pir backings based on build tags
    `go install "github.com/privacylab/talek/cli/talekreplica" -tags pircpu`

---
+++ pir/pircpu.go
@@ -0,0 +1,7 @@
+//  +build pircpu
+
+package pir
+
+import (
+	_ "github.com/privacylab/talek/pir/pircpu"
+)
---
+++ pir/pircuda.go
@@ -0,0 +1,7 @@
+//  +build pircuda
+
+package pir
+
+import (
+	_ "github.com/privacylab/talek/pir/pircuda"
+)
