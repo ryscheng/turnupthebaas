421c388f147c7d00500bf930cd1d29af24164893
Raymond Cheng <me@raymondcheng.net>
2017-05-16 11:02:22 -0700

    add a comment

--- pir/pircl.go
+++ pir/pircl.go
@@ -3,5 +3,6 @@
 package pir
 
 import (
+	// Trigger a dependency when the build tags are satisfied for `go install`
 	_ "github.com/privacylab/talek/pir/pircl"
 )
--- pir/pircpu.go
+++ pir/pircpu.go
@@ -1,5 +1,6 @@
 package pir
 
 import (
+	// Trigger a dependency when the build tags are satisfied for `go install`
 	_ "github.com/privacylab/talek/pir/pircpu"
 )
