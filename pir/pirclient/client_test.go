f1f4e7228a75fa9a26e9e3f08ee5a7a3d15f360a
Raymond Cheng <me@raymondcheng.net>
2017-06-02 18:18:06 -0700

    one more test

--- pir/pirclient/client_test.go
+++ pir/pirclient/client_test.go
@@ -29,7 +29,25 @@ func TestGenerateRequestVectors(t *testing.T) {
 	if result != 2 {
 		t.Errorf("Secret request vector should translate to 2, not %v", result)
 	}
+}
 
+func TestGenerateRequestVectorsOddNumBuckets(t *testing.T) {
+	c := NewClient("test")
+	reqVec, err := c.GenerateRequestVectors(1, 3, 65)
+	if err != nil {
+		t.Errorf("GenerateRequestVectors failed: %v", err)
+	}
+	if len(reqVec) != 3 {
+		t.Errorf("GenerateRequestVectors produced too few request vectors %v, expected 3", len(reqVec))
+	}
+	resultBytes, err := c.CombineResponses(reqVec)
+	if err != nil {
+		t.Errorf("CombineResponses failed: %v", err)
+	}
+	result, _ := binary.Uvarint(resultBytes)
+	if result != 2 {
+		t.Errorf("Secret request vector should translate to 2, not %v", result)
+	}
 }
 
 func TestGenerateRequestVectorsInvalidNumServers(t *testing.T) {
